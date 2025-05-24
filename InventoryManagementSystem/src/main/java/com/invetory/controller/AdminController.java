b6621d0f5dd627f08e1c42b22d8cb9d5ad6872f3
sandip nale <sandipnale143@gmail.com>
2025-05-24 16:20:01 +0530

    controll add

--- InventoryManagementSystem/src/main/java/com/invetory/controller/AdminController.java
+++ InventoryManagementSystem/src/main/java/com/invetory/controller/AdminController.java
@@ -1,5 +1,17 @@
 package com.invetory.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
 
+	@
+	public ResponseEntity<?> purchase(){
+		
+	}
+	
+	
 }
--- InventoryManagementSystem/src/main/java/com/invetory/controller/UserController.java
+++ InventoryManagementSystem/src/main/java/com/invetory/controller/UserController.java
@@ -1,5 +1,45 @@
 package com.invetory.controller;
 
-public class UserController {
+import java.util.List;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.invetory.entities.Product;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping("/allProduct")
+	public ResponseEntity<?>listOfProduct(){
+		return ResponseEntity.ok().body(null);
+	}
+
+	@PostMapping("/cart")
+	public ResponseEntity<?>addCart(Product p){
+		
+		return ResponseEntity.ok().body(p);
+		
+	}
+	
+	@DeleteMapping("/delete")
+	public ResponseEntity<?>removeFromCart(Long Id){
+		
+	return	ResponseEntity.ok().body(null);
+	}
+	
+	@PostMapping("/purchase")
+	public ResponseEntity<?>purchase(List<Product> l){
+		return	ResponseEntity.ok().body(null);
+	}
+	
+	
+	
 }
